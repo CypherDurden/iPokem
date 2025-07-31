da2610400ad9e97452f73fc1acf476e23598508f
me <apple.vitor@>
2025-07-31 19:52:47 -0300

    Initial Commit

---
+++ iPokem/iPokemApp.swift
@@ -0,0 +1,17 @@
+//
+//  iPokemApp.swift
+//  iPokem
+//
+//  Created by Vitor Lopes on 31/07/25.
+//
+
+import SwiftUI
+
+@main
+struct iPokemApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ iPokemTests/iPokemTests.swift
@@ -0,0 +1,17 @@
+//
+//  iPokemTests.swift
+//  iPokemTests
+//
+//  Created by Vitor Lopes on 31/07/25.
+//
+
+import Testing
+@testable import iPokem
+
+struct iPokemTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
